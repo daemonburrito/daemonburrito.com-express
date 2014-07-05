2fcd1468943b36c1409d27f348db03716a9d3d70
Scott Drake <demonburrito@gmail.com>
2014-07-05 14:31:53 -0500

    refresh_posts
    Impl to check for new or updated posts in configured markdown folders.
    * Added `post_paths` to `config`.
    * Added `refresh_posts` to `util`.
    * DB left to be done.
    * Added `db(fn)` convenience wrapper.
    * Lots of docs.

--- config.js
+++ config.js
@@ -1,5 +1,10 @@
+var path = require('path');
+
 module.exports = {
 	title: "Scott Drake's Blog",
 	byline: "The adventures of a Developer Philosopher",
+	post_paths: [path.join(__dirname, 'md')],
 	post: false
 };
+
+console.log(module.exports.posts_path);
--- util.js
+++ util.js
@@ -1,15 +1,82 @@
-var format_regex = /{(\d)}/g;
+// daemonburrito.com utils
 
+// Convenient, traditional String.format(). Takes an Array instead of using
+// arguments (faster).
+var format_regex = /{(\d)}/g;
 String.prototype.format = function (vals) {
 	return this.replace(format_regex, function (match, p) {
 		return vals[p];
 	});
 };
 
+
+// Make the connect string for pg. pg also uses this string as a key in its
+// table of connections.
 var make_pg_constring = function (user, pass, host, db) {
 	return 'postgres://{0}:{1}@{2}/{3}'.format(arguments);
 }
 
+
+// db connection
+var db = function (fn) {
+	var pg = require('pg'),
+	pg_user = process.env.BLOG_PG_USER,
+	pg_pass = process.env.BLOG_PG_PASS,
+	pg_host = process.env.BLOG_PG_HOST || 'localhost',
+	pg_db = process.env.BLOG_PG_DB,
+
+	constring = make_pg_constring(pg_user, pg_pass, pg_host, pg_db);
+
+	pg.connect(constring, fn);
+};
+
+
+// Refresh posts
+//
+// Look in the md directory and create or update posts in the db.
+var refresh_posts = function () {
+	// get a list of md files in the configured path
+	var fs = require('fs'),
+		_path = require('path'),
+		paths = Object.create(require('./config').post_paths),
+		all_files = [];
+
+	var compare_posts = function () {
+		console.log(all_files);
+		/*
+		db(function (err, client, done) {
+			client.query();
+		});
+	   */
+	};
+
+	paths.forEach(function (path) {
+		var files_in_path = [];
+
+		fs.readdir(path, function (err, files) {
+			if (err) {
+				console.log(err);
+			}
+
+			if (files) {
+				files_in_path = files
+			}
+
+			files_in_path.forEach(function (file) {
+				all_files.push(_path.join(path, file));
+			});
+
+			paths.pop();
+
+			if (paths.length === 0) {
+				compare_posts();
+			}
+		});
+	});
+};
+
+
 module.exports = {
-	make_pg_constring: make_pg_constring
+	make_pg_constring: make_pg_constring,
+	refresh_posts: refresh_posts
 }
